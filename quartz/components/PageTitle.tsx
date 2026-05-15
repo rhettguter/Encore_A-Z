83bcbf67927cdd65cb81c3e3e31a90df471115a9
rhettguter <gutermail@gmail.com>
2026-05-15 01:08:10 +0100

    Update PageTitle.tsx

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -12,7 +12,7 @@ const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzCompo
     <h2 class={classNames(displayClass, "page-title")}>
       <a href={baseDir} aria-label={title}>
         <img
-          src={`${baseDir}static/encore-logo.svg`}
+          src={`${baseDir}/static/encore-logo.svg`}
           alt={title}
           class="page-title-logo"
         />
